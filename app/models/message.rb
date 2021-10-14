61869f2a46a3372bcea6dae749118a63c44cf1b8
Douglas Goncalves <dougmarques10@gmail.com>
2021-10-14 19:16:46 -0300

    create code from actioncable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  belongs_to :messagable, polymorphic: true
+  belongs_to :user
+  validates :body, :user, presence: true
+
+  after_create_commit { MessageBroadcastJob.perform_later self }
 end
